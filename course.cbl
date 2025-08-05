05644c487710cca7a0b996560469b6882432b5b2
Abade Francisco <francisco.a.filho@iesb.edu.br>
2025-08-04 23:54:54 -0300

    COBOL DAY TWO

---
+++ course.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "course".
+       AUTHOR.     FRNC-ABADE
+       
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+       0100-START-HERE.
+           DISPLAY "Hello-world".
+           STOP RUN.
+       END PROGRAM course.    
+    
